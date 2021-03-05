9d24f853708eb3924f10bee72b8dccb6941351b0
Kilson <kilsonrs@gmail.com>
2021-03-05 16:22:07 -0300

    chore: add a SignIn page

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 
+import { Form } from '@unform/web';
+
+import Logo from '../../assets/logo.png';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 import { Container } from './styles';
 
+interface SignInFormData {
+  email: string;
+  password: string;
+}
+
 const SignIn: React.FC = () => {
+  const handleSubmit = useCallback((data: SignInFormData) => {
+    const { email, password } = data;
+    console.log(email, password);
+  }, []);
   return (
     <Container>
-      <h1>SignIn</h1>
+      <Form onSubmit={handleSubmit}>
+        <img src={Logo} alt="FastFeet" />
+
+        <strong>SEU E-MAIL</strong>
+        <Input name="email" />
+
+        <strong>SUA SENHA</strong>
+        <Input name="password" type="password" />
+
+        <Button type="submit">Entrar no sistema</Button>
+      </Form>
     </Container>
   );
 };
--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,3 +1,42 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  height: 100vh;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: #7d40e7;
+
+  form {
+    height: 425px;
+    width: 360px;
+    padding: 30px;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    border-radius: 4px;
+    box-shadow: 0px 0px 10px #00000033;
+    background: #ffffff;
+
+    img {
+      max-width: 250px;
+      width: 100%;
+      margin: 0 auto 40px;
+    }
+
+    strong {
+      margin-bottom: 9px;
+      color: #444444;
+    }
+
+    strong:nth-child(4) {
+      margin: 15px 0 9px;
+    }
+
+    button {
+      margin-top: 15px;
+    }
+  }
+`;
