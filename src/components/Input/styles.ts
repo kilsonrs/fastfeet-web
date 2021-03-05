27aede0b21f705b107c53254dae64a8d54aa187a
Kilson <kilsonrs@gmail.com>
2021-03-05 16:15:14 -0300

    chore: add Input component

---
+++ src/components/Input/styles.ts
@@ -0,0 +1,38 @@
+import styled, { css } from 'styled-components';
+
+interface ContainerProps {
+  isErrored: boolean;
+  isFilled: boolean;
+  isFocus: boolean;
+}
+
+export const Container = styled.div<ContainerProps>`
+  display: flex;
+  align-items: center;
+  border: 1px solid #dddddd;
+  border-radius: 4px;
+  padding: 12px 15px;
+
+  ${props =>
+    props.isFocus &&
+    css`
+      border-color: #7d40e7;
+    `}
+
+  ${props =>
+    props.isErrored &&
+    css`
+      border-color: #de3b3b;
+    `}
+
+  ${props =>
+    props.isFilled &&
+    css`
+      border-color: #7d40e7;
+    `}
+
+  input {
+    border: 0;
+    color: #999999;
+  }
+`;
