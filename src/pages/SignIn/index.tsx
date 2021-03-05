46d117f0427ddf72b46db461387a1c579d1dcbd9
Kilson <kilsonrs@gmail.com>
2021-03-05 18:04:34 -0300

    feat: add SignIn page feature

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,11 +1,15 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useRef } from 'react';
 
 import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
+import * as Yup from 'yup';
 
 import Logo from '../../assets/logo.png';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 import { Container } from './styles';
+import getValidationErrors from '../../utils/getValidationErrors';
+import api from '../../services/api';
 
 interface SignInFormData {
   email: string;
@@ -13,13 +17,34 @@ interface SignInFormData {
 }
 
 const SignIn: React.FC = () => {
-  const handleSubmit = useCallback((data: SignInFormData) => {
-    const { email, password } = data;
-    console.log(email, password);
+  const formRef = useRef<FormHandles>(null);
+
+  const handleSubmit = useCallback(async (data: SignInFormData) => {
+    try {
+      formRef.current?.setErrors({});
+
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .required('E-mail obrigatório')
+          .email('Digite um e-mail válido'),
+        password: Yup.string().min(8, 'Pelo menos 8 dígitos'),
+      });
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+      await api.post('/users', data);
+    } catch (err) {
+      if (err instanceof Yup.ValidationError) {
+        const errors = getValidationErrors(err);
+
+        formRef.current?.setErrors(errors);
+      }
+    }
   }, []);
+
   return (
     <Container>
-      <Form onSubmit={handleSubmit}>
+      <Form onSubmit={handleSubmit} ref={formRef}>
         <img src={Logo} alt="FastFeet" />
 
         <strong>SEU E-MAIL</strong>
