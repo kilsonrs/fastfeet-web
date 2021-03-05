404065ae31f02cef3f7839638e78b0c8eb3b72b3
Kilson <kilsonrs@gmail.com>
2021-03-05 16:15:01 -0300

    chore: add Button component

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container = styled.button`
+  border: 0;
+  border-radius: 4px;
+  background: #7d40e7;
+  padding: 12px 0;
+
+  color: #fff;
+  font-weight: 500;
+`;
