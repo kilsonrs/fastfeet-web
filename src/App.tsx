53fc4b5cdad97549105e1c69bf714068e074ab80
Kilson <kilsonrs@gmail.com>
2021-03-05 12:40:12 -0300

    chore: add initial Route

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Routes from './routes';
 
 const App: React.FC = () => {
-  return <div />;
+  return (
+    <Router>
+      <Routes />
+    </Router>
+  );
 };
 
 export default App;
