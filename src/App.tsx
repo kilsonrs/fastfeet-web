344e6d2a52de28348fca914907c9c1878a485a9f
Kilson <kilsonrs@gmail.com>
2021-03-08 16:21:00 -0300

    chore: import AuthProvider to App

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import GlobalStyle from './styles/global';
 import Routes from './routes';
+import AppProvider from './hooks';
 
 const App: React.FC = () => {
   return (
     <Router>
-      <Routes />
+      <AppProvider>
+        <Routes />
+      </AppProvider>
       <GlobalStyle />
     </Router>
   );
