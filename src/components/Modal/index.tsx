df296ded7fb9dc57b05296247bcd2d3513f7b6e6
Kilson <kilsonrs@gmail.com>
2021-04-09 12:37:17 -0300

    chore: move modal style to a variable and set ariaHideApp to false

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -6,26 +6,29 @@ interface ModalProps {
   setIsOpen(): void;
 }
 
+const modalStyle = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    border: 'none',
+    padding: '25px',
+  },
+  overlay: {
+    backgroundColor: 'rgba(0,0,0, 0.8)',
+  },
+};
+
 const Modal: React.FC<ModalProps> = ({ children, isOpen, setIsOpen }) => {
   return (
     <ReactModal
       isOpen={isOpen}
       onRequestClose={setIsOpen}
-      style={{
-        content: {
-          top: '50%',
-          left: '50%',
-          right: 'auto',
-          bottom: 'auto',
-          marginRight: '-50%',
-          transform: 'translate(-50%, -50%)',
-          border: 'none',
-          padding: '25px',
-        },
-        overlay: {
-          backgroundColor: 'rgba(0,0,0, 0.8)',
-        },
-      }}
+      style={modalStyle}
+      ariaHideApp={false}
     >
       {children}
     </ReactModal>
